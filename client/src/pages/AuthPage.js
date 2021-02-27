9508bec170e771357bd967649f98411419a234be
SoloProger <andreisolo.lite@gmail.com>
2021-02-27 11:08:13 +0300

    #main create full Auth-system on back

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useHttp} from '../hooks/http.hook'
+import {useMessage} from "../hooks/message.hook";
 
 export const AuthPage = () => {
-    // const [form, setForm] = useState({
-    //     email: '', password: ''
-    // })
+    const message = useMessage()
+    const {loading, error, request, clearError} = useHttp()
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
 
-    // const changeHandler
+    useEffect( () => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try{
+            const data = await request('/api/auth/register', 'POST', {...form})
+            message(data.message)
+        } catch (e){}
+    }
+
+    const loginHandler = async () => {
+        try{
+            const data = await request('/api/auth/login', 'POST', {...form})
+            message(data.message)
+        } catch (e){}
+    }
 
     return (
         <div className="row">
@@ -23,6 +48,7 @@ export const AuthPage = () => {
                                     type="text"
                                     name="email"
                                     className="yellow-input"
+                                    onChange={changeHandler}
                                 />
                                     <label htmlFor="email">Email</label>
                             </div>
@@ -31,9 +57,10 @@ export const AuthPage = () => {
                                 <input
                                     placeholder="Введите пароль"
                                     id="password"
-                                    type="text"
+                                    type="password"
                                     name="password"
                                     className="yellow-input"
+                                    onChange={changeHandler}
                                 />
                                 <label htmlFor="password">Пароль</label>
                             </div>
@@ -41,8 +68,21 @@ export const AuthPage = () => {
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn yellow darken-4" style={{marginRight: 10}}>Войти</button>
-                        <button className="btn grey lighten-1 black-text">Регистрация</button>
+                        <button
+                            className="btn yellow darken-4"
+                            style={{marginRight: 10}}
+                            disabled={loading}
+                            onClick={loginHandler}
+                        >
+                            Войти
+                        </button>
+                        <button
+                            className="btn grey lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}
+                        >
+                            Регистрация
+                        </button>
                     </div>
                 </div>
             </div>
